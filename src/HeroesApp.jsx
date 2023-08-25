336d3db52f219609d1b9e8822681e70da7e9c177
Agustin <zagoagus@gmail.com>
2023-08-24 21:04:18 -0300

    initial commit

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppRouter } from './router/AppRouter'
+import { Navbar } from './ui/components/Navbar'
+
+export const HeroesApp = () => {
+  return (
+    <>
+        <AppRouter/>
+    </>
+  )
+}
